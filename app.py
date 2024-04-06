234edaaf64128c611a6b69072ef0967cb5dba16b
Siftain Ahmad <100927094+SiftainAhmad@users.noreply.github.com>
2024-04-06 06:10:45 +0530

    app.py

--- app.py
+++ app.py
@@ -27,11 +27,11 @@ movies = pd.DataFrame(movies_dict)
 # Main UI
 st.markdown("<h1 style='color: #000000; background-color: #34AABD; padding: 10px; text-align: center; margin-bottom: 0; font-size: 35px;'>🎬 Movie Recommender</h1>", unsafe_allow_html=True)
 selected_movie = st.selectbox("Type or select a movie from the dropdown", movies['title'].values)
-num_movies_str = st.text_input("Select the number of related movies (max 10)", "5")
+num_movies_str = st.text_input("Select the number of related movies (Max 10)", "0")
 num_movies = int(num_movies_str)
 
 # Show recommendations on button click
-if st.button('Show Recommendation'):
+if st.button('Show Recommendation') or st.text_input:
     recommended_movies = recommend(selected_movie, num_movies)
     
     # Display recommended movies with posters and IMDb links in grid layout
